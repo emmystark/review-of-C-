47e27d4ec1b1a2bee1ab77e348991a2d3e68f6c8
emmystark <nkereuwememmanuel03@gmail.com>
2025-08-21 23:06:08 +0100

    compiling c++

--- ReadME.md
+++ ReadME.md
@@ -1,3 +1,18 @@
 # Activating env 
 
 source ~/myenv/bin/activate
+
+
+
+# Compiling the file 
+g++ start.cpp -o start
+
+
+# Running the file 
+./start
+
+
+# Compiling and Running together 
+
+
+g++ start.cpp -o start && ./start
--- start.cpp
+++ start.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  cout << "How are you doing today?" << endl;
+  cout << "I hope you are doing well." << endl;
+  cout << "Have a great day!" << endl;
+  cout << "Goodbye!" << endl;
+  cout << "See you later!" << endl;
+  cout << "Take care!" << endl;
+  cout << "Stay safe!" << endl;
+  cout << "Keep smiling!" << endl;
+  cout << "Remember to be kind!" << endl;
+//   cout << "Spread positivity!" << endl;
+//   cout << "Enjoy your day!" << endl;
+//   cout << "Make the most of it!" << endl;
+//   cout << "Cherish every moment!" << endl;
+//   cout << "Live life to the fullest!" << endl;
+  return 0;
 }
